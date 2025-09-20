0abd6b237bfed6cfde99b76711111eaad067808c
Dylan Corr <dylancorr.g@gmail.com>
2025-09-20 09:32:32 +0100

    Deploy EC2 WordPress in private subnet behind public ALB

--- infrastructure/lib/InfrastructureStack.ts
+++ infrastructure/lib/InfrastructureStack.ts
@@ -2,6 +2,8 @@ import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as ec2 from "aws-cdk-lib/aws-ec2";
 import * as iam from "aws-cdk-lib/aws-iam";
+import * as elbv2 from "aws-cdk-lib/aws-elasticloadbalancingv2";
+import * as targets from "aws-cdk-lib/aws-elasticloadbalancingv2-targets";
 
 export class InfrastructureStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -10,6 +12,8 @@ export class InfrastructureStack extends cdk.Stack {
     // Create a new VPC with 2 Availability Zones
     const vpc = new ec2.Vpc(this, "Vpc", {
       maxAzs: 2,
+
+      natGateways: 1,
 
       // Define subnets to be created in the VPC
       subnetConfiguration: [
@@ -20,28 +24,26 @@ export class InfrastructureStack extends cdk.Stack {
         },
         {
           name: "Private",
-          subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+          subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
           cidrMask: 24,
         },
       ],
     });
 
     //Sceurity Groups
-    const ec2SecurityGroup = new ec2.SecurityGroup(this, "EC2SecurityGroup", {
-      vpc: vpc,
+    const albSG = new ec2.SecurityGroup(this, "ALBSecurityGroup", {
+      vpc,
+      description: "Allow HTTP from internet",
       allowAllOutbound: true,
-      description: "Security group for WP-Instance in public subnet",
     });
-    ec2SecurityGroup.addIngressRule(
-      ec2.Peer.anyIpv4(),
-      ec2.Port.tcp(443),
-      "Allow HTTPS"
-    );
-    ec2SecurityGroup.addIngressRule(
-      ec2.Peer.anyIpv4(),
-      ec2.Port.tcp(80),
-      "Allow HTTP"
-    );
+    albSG.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(80), "Allow HTTP");
+
+    const ec2SG = new ec2.SecurityGroup(this, "EC2SecurityGroup", {
+      vpc,
+      description: "Allow HTTP from ALB only",
+      allowAllOutbound: true,
+    });
+    ec2SG.addIngressRule(albSG, ec2.Port.tcp(80), "Allow ALB");
 
     //User Data Script on boot of WP-Instance
     const userData = ec2.UserData.forLinux();
@@ -63,11 +65,11 @@ export class InfrastructureStack extends cdk.Stack {
       "sudo chmod -R 755 /var/www/html"
     );
 
-    //Create EC2 instance in public subnet
+    //Create EC2 instance in private subnet
     const instance1 = new ec2.Instance(this, "WP-Instance", {
       vpc: vpc,
       vpcSubnets: {
-        subnetType: ec2.SubnetType.PUBLIC,
+        subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
       },
 
       instanceType: ec2.InstanceType.of(
@@ -75,8 +77,26 @@ export class InfrastructureStack extends cdk.Stack {
         ec2.InstanceSize.MICRO
       ),
       machineImage: ec2.MachineImage.latestAmazonLinux2023(),
-      securityGroup: ec2SecurityGroup,
+      securityGroup: ec2SG,
       userData: userData,
+    });
+
+    //Public Application Load Balancer
+    const alb = new elbv2.ApplicationLoadBalancer(this, "ALB", {
+      vpc,
+      internetFacing: true,
+      securityGroup: albSG,
+    });
+
+    const listener = alb.addListener("Listener", {
+      port: 80,
+      open: true,
+    });
+
+    listener.addTargets("WPTargets", {
+      port: 80,
+      targets: [new targets.InstanceTarget(instance1)],
+      healthCheck: { path: "/" },
     });
 
     //Outputs of CDK Stack
@@ -84,12 +104,8 @@ export class InfrastructureStack extends cdk.Stack {
       value: vpc.vpcId,
     });
 
-    new cdk.CfnOutput(this, "InstancePublicIp", {
-      value: instance1.instancePublicIp,
-    });
-
-    new cdk.CfnOutput(this, "InstancePublicDns", {
-      value: instance1.instancePublicDnsName,
+    new cdk.CfnOutput(this, "ALB-DNS", {
+      value: alb.loadBalancerDnsName,
     });
   }
 }

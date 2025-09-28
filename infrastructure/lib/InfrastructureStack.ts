be41d1f22a9bdc13b6d7a1a236cd8872f1acafc5
Dylan Corr <dylancorr.g@gmail.com>
2025-09-28 15:45:57 +0100

    Deployed RDS MySQL instance with Secrets Manager credentials Provisioned EC2 instance with UserData to install WordPress Configured ALB for public access

--- infrastructure/lib/InfrastructureStack.ts
+++ infrastructure/lib/InfrastructureStack.ts
@@ -4,6 +4,7 @@ import * as ec2 from "aws-cdk-lib/aws-ec2";
 import * as iam from "aws-cdk-lib/aws-iam";
 import * as elbv2 from "aws-cdk-lib/aws-elasticloadbalancingv2";
 import * as targets from "aws-cdk-lib/aws-elasticloadbalancingv2-targets";
+import * as rds from "aws-cdk-lib/aws-rds";
 
 export class InfrastructureStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -38,6 +39,7 @@ export class InfrastructureStack extends cdk.Stack {
     });
     albSG.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(80), "Allow HTTP");
 
+    //EC2 Secuirty Group
     const ec2SG = new ec2.SecurityGroup(this, "EC2SecurityGroup", {
       vpc,
       description: "Allow HTTP from ALB only",
@@ -45,24 +47,66 @@ export class InfrastructureStack extends cdk.Stack {
     });
     ec2SG.addIngressRule(albSG, ec2.Port.tcp(80), "Allow ALB");
 
+    //RDS Secuirty Group
+    const rdsSG = new ec2.SecurityGroup(this, "RDSSecuirtyGroup", {
+      vpc,
+      description: "Allow EC2 to connect to RDS on port 3306",
+      allowAllOutbound: true,
+    });
+    rdsSG.addIngressRule(ec2SG, ec2.Port.tcp(3306), "Allow EC2SG");
+
+    // Create the RDS Instance
+    const rdsinstance = new rds.DatabaseInstance(this, "rdsInstance", {
+      engine: rds.DatabaseInstanceEngine.mysql({
+        version: rds.MysqlEngineVersion.VER_8_0,
+      }),
+      vpc: vpc,
+      vpcSubnets: { subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS },
+      instanceType: ec2.InstanceType.of(
+        ec2.InstanceClass.BURSTABLE3,
+        ec2.InstanceSize.MICRO
+      ),
+      allocatedStorage: 20,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      securityGroups: [rdsSG],
+      credentials: rds.Credentials.fromGeneratedSecret("admin"),
+      databaseName: "wordpress",
+    });
+
     //User Data Script on boot of WP-Instance
     const userData = ec2.UserData.forLinux();
 
     userData.addCommands(
+      // Install all dependencies
       "sudo dnf update -y",
-      "sudo dnf install -y httpd",
-      "sudo systemctl start httpd",
-      "sudo systemctl enable httpd",
-      "sudo systemctl status httpd",
-      "sudo dnf install -y php php-mysqlnd php-fpm php-json php-gd wget unzip mariadb105",
-      "sudo systemctl restart httpd",
+      "sudo dnf install -y httpd php php-mysqlnd php-fpm php-json php-gd wget unzip mariadb105 awscli jq",
+      "sudo systemctl enable --now httpd",
+      "sudo systemctl enable --now php-fpm",
+
+      // Download and install WordPress
       "cd /var/www/html",
       "sudo wget https://wordpress.org/latest.tar.gz",
       "sudo tar -xzf latest.tar.gz",
       "sudo mv wordpress/* .",
       "sudo rm -rf wordpress latest.tar.gz",
       "sudo chown -R apache:apache /var/www/html",
-      "sudo chmod -R 755 /var/www/html"
+      "sudo chmod -R 755 /var/www/html",
+
+      // Fetch DB credentials from Secrets Manager
+      `DB_SECRET=$(aws secretsmanager get-secret-value --secret-id ${
+        rdsinstance.secret!.secretArn
+      } --query SecretString --output text --region ${this.region})`,
+      "DB_USER=$(echo $DB_SECRET | jq -r .username)",
+      "DB_PASS=$(echo $DB_SECRET | jq -r .password)",
+      "DB_HOST=$(echo $DB_SECRET | jq -r .host)",
+      "DB_NAME=$(echo $DB_SECRET | jq -r .dbname)",
+
+      // Configure WordPress wp-config.php
+      "cp wp-config-sample.php wp-config.php",
+      'sed -i "s/database_name_here/$DB_NAME/" wp-config.php',
+      'sed -i "s/username_here/$DB_USER/" wp-config.php',
+      'sed -i "s/password_here/$DB_PASS/" wp-config.php',
+      'sed -i "s/localhost/$DB_HOST/" wp-config.php'
     );
 
     //Create EC2 instance in private subnet
@@ -73,7 +117,7 @@ export class InfrastructureStack extends cdk.Stack {
       },
 
       instanceType: ec2.InstanceType.of(
-        ec2.InstanceClass.T2,
+        ec2.InstanceClass.T3,
         ec2.InstanceSize.MICRO
       ),
       machineImage: ec2.MachineImage.latestAmazonLinux2023(),
@@ -81,7 +125,18 @@ export class InfrastructureStack extends cdk.Stack {
       userData: userData,
     });
 
+    //Permissions
+
+    //Grant permissiom for EC2 instance to read from RDS
+    rdsinstance.secret?.grantRead(instance1.role);
+
+    // Enable SSM Session Manager for EC2
+    instance1.role.addManagedPolicy(
+      iam.ManagedPolicy.fromAwsManagedPolicyName("AmazonSSMManagedInstanceCore")
+    );
+
     //Public Application Load Balancer
+
     const alb = new elbv2.ApplicationLoadBalancer(this, "ALB", {
       vpc,
       internetFacing: true,
